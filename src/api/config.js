7c5255d3f6149fee5e460258610bf667f71c32b0
minwook-Kang <yjo2567@gmail.com>
2026-06-24 17:09:58 +0900

    ALB DNS

--- src/api/config.js
+++ src/api/config.js
@@ -1,4 +1,4 @@
-const DEFAULT_API_BASE_URL = "http://13.214.156.69:8080";
+const DEFAULT_API_BASE_URL = "http://user98-backend-alb-1733908966.ap-southeast-1.elb.amazonaws.com";
 
 export const API_BASE_URL = (
   import.meta.env.VITE_API_BASE_URL || DEFAULT_API_BASE_URL
