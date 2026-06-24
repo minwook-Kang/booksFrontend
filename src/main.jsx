88be5258c65dd3bfcab3a7442ffea602204e1316
minwook-Kang <yjo2567@gmail.com>
2026-06-24 10:39:07 +0900

    main ch

--- src/main.jsx
+++ src/main.jsx
@@ -9,8 +9,8 @@ import App from './App.jsx'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     {/* App 내부에서 URL 기반 라우팅을 사용할 수 있도록 설정 */}
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
 )
